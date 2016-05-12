a32d71deb27c8ede4d932916d7e7a36eb83c285e
brujua <brunocrisafulli@yahoo.com.ar>
2016-05-12 14:39:06 -0300

    dos boludeces
    agregre dos lineas

--- Jodete/src/modelo/Game.java
+++ Jodete/src/modelo/Game.java
@@ -1,7 +1,8 @@
 package modelo;
 
 public class Game {
-	SpanishDeck 
+	SpanishDeck deck;
+	DeckGraveyard graveYard;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
